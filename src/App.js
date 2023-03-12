a941d60cab47d9480de6d1b385e02174493013c5
Keeanu Rodriguez <keeanu7@gmail.com>
2023-03-12 19:38:34 -0400

    error modal added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
-import Home from "./components/Home"
-import Nav from "./components/Nav"
-import About from "./components/About"
+import Home from "./Components/Home"
+import Nav from "./Components/Nav"
+import About from "./Components/About"
+import ErrorModal from "./Components/ErrorModal"
 import {getVideos} from "./api/fetch.js"
 import './App.css';
 
 import { teamData } from "./data/team.js";
 
 function App() {
-const [allVideos, setAllVideos] = useState([])
-  
-useEffect(() => {
-    getVideos().then(response => {
-      setAllVideos(response)
-    }).catch((error) => console.log(error))
-  }, [])
+const [allVideos, setAllVideos] = useState([]);
+const [modal , setModal] = useState(false);
+// useEffect(() => {
+//     getVideos().then(response => {
+//       setAllVideos(response)
+//     }).catch((error) => console.log(error))
+//   }, [])
 
   return (
     <div className="App">
      <Router>
         <Nav />
       <Routes>
-        <Route path="/" element={<Home allVideos={allVideos}/>} />
+        <Route path="/" element={<Home allVideos={allVideos} modal={modal} setModal={setModal}/>} />
         <Route path="/about" element={<About teamData={teamData}/>} />
       </Routes>
      </Router>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,20 @@
-export default function Home({allVideos}) {
-    console.log(allVideos);
+import { useState } from "react";
+import ErrorModal from "./ErrorModal"
+
+export default function Home({allVideos, modal, setModal}) {
+    //const [modal , setModal] = useState(false);
+    //setModal(true)
+    function toggleModal () {
+        setModal(!modal)
+    }
+
+    //console.log(modal);
     return (
         <div>
+            <form>
+                <input type="text" />
+            </form>
+            <ErrorModal toggleModal={toggleModal} modal={modal}/>
             {/* {allVideos.items.map((video) => {return (
                 <h1>{video.etag}</h1>
             )})} */}
