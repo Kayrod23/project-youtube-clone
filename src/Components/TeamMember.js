d20dd0f6dfb6232d860c950d14d1da148bbc074d
christian orlando <christianorlando@pursuit.org>
2023-03-13 11:46:49 -0400

    saving commit

--- src/Components/TeamMember.js
+++ src/Components/TeamMember.js
@@ -2,13 +2,14 @@ import { Link } from "react-router-dom";
 import christian from "../data/christian.jpg";
 import carlos from "../data/carlos.png";
 import keeanu from "../data/keeanu.png";
+import isaiah from "../data/isaiah.jpeg";
 
 const TeamMember = ({ member }) => {
     let ourimg;
     let name = `${member.firstName} ${member.lastName}`;
     switch (member.id) {
         case "CP-1":  ourimg = carlos; break;
-        case "ID-2":   ourimg = carlos; break;
+        case "ID-2":   ourimg = isaiah; break;
         case "KR-3":  ourimg = keeanu; break;
         case "CO-4":  ourimg = christian; break;
         default: break;
