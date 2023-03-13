bb2c71164723479d2996486f743f1f27c816b2b3
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-03-13 10:59:04 -0400

    Add state to search bar.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
-import Home from "./components/Home"
-import Nav from "./components/Nav"
-import About from "./components/About"
-import Member from  "./components/Member"
-import {getVideos} from "./api/fetch.js"
-import './App.css';
+import Home from "./Components/Home";
+import Nav from "./Components/Nav";
+import About from "./Components/About";
+import Member from "./Components/Member";
+import { getVideos } from "./api/fetch.js";
+import "./App.css";
 
 import { teamData } from "./data/team.js";
 
 function App() {
-const [allVideos, setAllVideos] = useState([])
-  
-useEffect(() => {
-    getVideos().then(response => {
-      setAllVideos(response)
-    }).catch((error) => console.log(error))
-  }, [])
+  const [allVideos, setAllVideos] = useState([]);
+
+  useEffect(() => {
+    getVideos()
+      .then((response) => {
+        setAllVideos(response);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <div className="App">
-     <Router>
+      <Router>
         <Nav />
-      <Routes>
-        <Route path="/" element={<Home allVideos={allVideos}/>} />
-        <Route path="/about" element={<About teamData={teamData}/>} />
-        <Route path="/about/:id" element={<Member  teamData={teamData} />} />
-      </Routes>
-     </Router>
+        <Routes>
+          <Route path="/" element={<Home allVideos={allVideos} />} />
+          <Route path="/about" element={<About teamData={teamData} />} />
+          <Route path="/about/:id" element={<Member teamData={teamData} />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,8 +1,20 @@
+import { useState } from "react";
 export const SearchBar = ({}) => {
+  const [val, setVal] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: use setQueue to add a new help request to the queue
+  };
+
+  const handleTextChange = (e) => {
+    setVal(e.target.value);
+  };
+
   return (
-    <form>
-      <input type="text"></input>
-      <button>Search!</button>
+    <form onSubmit={handleSubmit}>
+      <input type="text" onChange={handleTextChange}></input>
+      <input type="submit" value="Search" />
     </form>
   );
 };
