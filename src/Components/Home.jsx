e89cc5465dae3e517c96689d23ef59e97adf0020
christian orlando <christianorlando@pursuit.org>
2023-03-13 21:21:13 -0400

    PR #3 Christian/IsaiahPic-ListVideoss

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import ErrorModal from "./ErrorModal"
+import VideosListing from "./VideosListing"
+import "./Home.css";
 
 export default function Home() {
     const [allVideos, setAllVideos] = useState([]);
@@ -9,24 +11,27 @@ export default function Home() {
         setModal(!modal)
     }
     
-    // fetch (`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${search}&key=${process.env.REACT_APP_API_KEY}`)
-    // .then((results) => results.json())
-    // .then(response => {
-    //     setAllVideos(response.items);
-    //     setModal(false);
-    // })
-    // .catch((error) => {
-    //     console.log(error);
-    //     setModal(true);
-    // })
+  // Only for testing
+    useEffect(() => {
+    fetch (`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&type=video&q=ariana grande&key=${process.env.REACT_APP_API_KEY}`)
+    .then((results) => results.json())
+    .then(response => {
+        setAllVideos(response.items);
+        setModal(false);
+    })
+    .catch((error) => {
+        console.log(error);
+        setModal(true);
+    })},[])
+// Only for testing
 
     return (
         <div>
             {/* <SearchBar /> */}
             <ErrorModal toggleModal={toggleModal} modal={modal}/>
-            {/* {allVideos.items.map((video) => {return (
-                <h1>{video.etag}</h1>
-            )})} */}
+            <section className="searchedvideos">
+                 {allVideos.map((video) => <VideosListing video={video}  key={video.id.videoId} />)}
+            </section>
         </div>
     )
 }
